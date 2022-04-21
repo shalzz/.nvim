7d5c96c29804482e83939658cf75801ee081eee8
Shaleen Jain <shaleen@jain.sh>
2022-04-21 07:23:15 +0000

    extract zk setup to zk-setup.lua

--- init.lua
+++ init.lua
@@ -492,33 +492,7 @@ require('lint').linters_by_ft = {
   markdown          = {'vale'}
 }
 
-require("zk").setup({
-  -- can be "telescope", "fzf" or "select" (`vim.ui.select`)
-  -- it's recommended to use "telescope" or "fzf"
-  picker = "telescope",
-
-  lsp = {
-    -- `config` is passed to `vim.lsp.start_client(config)`
-    config = {
-      cmd = { "zk", "lsp" },
-      name = "zk",
-      -- on_attach = ...
-      -- etc, see `:h vim.lsp.start_client()`
-    },
-
-    -- automatically attach buffers in a zk notebook that match the given filetypes
-    auto_attach = {
-      enabled = true,
-      filetypes = { "markdown" },
-    },
-  },
-})
-
-local dir = vim.fn.getenv('ZK_NOTEBOOK_DIR')..'/journal/daily'
-vim.keymap.set("n", "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", {silent = false})
-vim.keymap.set("n", "<leader>zjy", string.format("<Cmd>ZkNew { dir = '%s', date = 'yesterday' }<CR>", dir), {silent = false})
-vim.keymap.set("n", "<leader>zjt", string.format("<Cmd>ZkNew { dir = '%s' }<CR>", dir), {silent = false})
-
 require('autocmd')
 require('keybindings')
+require('zk-setup')
 -- vim: ts=2 sts=2 sw=2 et
